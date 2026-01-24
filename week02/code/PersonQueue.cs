51c29a9f6510a09df0cec885e98896ccf9006580
ivanakena83 <akenaivan004@gmail.com>
2026-01-24 13:36:15 +0300

    Refactor PersonQueue to use Queue<T> instead of List<T>

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -1,35 +1,18 @@
-/// <summary>
-/// A basic implementation of a Queue
-/// </summary>
+using System.Collections.Generic;
+
 public class PersonQueue
 {
-    private readonly List<Person> _queue = new();
+    private Queue<Person> _queue = new Queue<Person>();
 
     public int Length => _queue.Count;
 
-    /// <summary>
-    /// Add a person to the queue
-    /// </summary>
-    /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Insert(0, person);
+        _queue.Enqueue(person);
     }
 
     public Person Dequeue()
     {
-        var person = _queue[0];
-        _queue.RemoveAt(0);
-        return person;
-    }
-
-    public bool IsEmpty()
-    {
-        return Length == 0;
-    }
-
-    public override string ToString()
-    {
-        return $"[{string.Join(", ", _queue)}]";
+        return _queue.Dequeue();
     }
 }
