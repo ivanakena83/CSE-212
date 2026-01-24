9c0d813ad588b52bd381a66383d4a93b4be1dd7c
ivanakena83 <akenaivan004@gmail.com>
2026-01-24 13:40:44 +0300

    Update TakingTurnsQueue.cs

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,57 +1,48 @@
-/// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
-/// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
-/// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
-/// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
-/// not be added back into the queue.
-/// </summary>
-public class TakingTurnsQueue
+using System;
+using System.Collections.Generic;
+
+public class PriorityQueue
 {
-    private readonly PersonQueue _people = new();
-
-    public int Length => _people.Length;
-
-    /// <summary>
-    /// Add new people to the queue with a name and number of turns
-    /// </summary>
-    /// <param name="name">Name of the person</param>
-    /// <param name="turns">Number of turns remaining</param>
-    public void AddPerson(string name, int turns)
+    private class QueueItem
     {
-        var person = new Person(name, turns);
-        _people.Enqueue(person);
+        public string Value { get; }
+        public int Priority { get; }
+
+        public QueueItem(string value, int priority)
+        {
+            Value = value;
+            Priority = priority;
+        }
     }
 
-    /// <summary>
-    /// Get the next person in the queue and return them. The person should
-    /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
-    /// if the queue is empty.
-    /// </summary>
-    public Person GetNextPerson()
+    private List<QueueItem> _items = new List<QueueItem>();
+
+    public void Enqueue(string value, int priority)
     {
-        if (_people.IsEmpty())
+        _items.Add(new QueueItem(value, priority));
+    }
+
+    public string Dequeue()
+    {
+        if (_items.Count == 0)
         {
-            throw new InvalidOperationException("No one in the queue.");
+            throw new InvalidOperationException("The queue is empty.");
         }
-        else
+
+        int highestPriorityIndex = 0;
+
+        for (int i = 1; i < _items.Count; i++)
         {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
+            if (_items[i].Priority > _items[highestPriorityIndex].Priority)
             {
-                person.Turns -= 1;
-                _people.Enqueue(person);
+                highestPriorityIndex = i;
             }
-
-            return person;
         }
-    }
 
-    public override string ToString()
-    {
-        return _people.ToString();
+        string value = _items[highestPriorityIndex].Value;
+        _items.RemoveAt(highestPriorityIndex);
+        return value;
     }
+}
+
 }
