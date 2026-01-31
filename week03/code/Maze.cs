b837ba6a1197ecd72b39fa6c26f5d13e0ba90fd1
akenaivan12 <akenaivan004@gmail.com>
2026-01-31 21:01:48 +0300

    week03

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,47 +1,55 @@
+using System;
+using System.Collections.Generic;
+
 public class Maze
 {
-    private Dictionary<(int x, int y), (bool Left, bool Right, bool Up, bool Down)> _maze;
+    private Dictionary<(int x, int y), bool[]> _maze;
     private (int x, int y) _currentPosition;
 
-    public Maze(
-        Dictionary<(int x, int y), (bool Left, bool Right, bool Up, bool Down)> maze,
-        (int x, int y) startPosition)
+    public Maze(Dictionary<(int, int), bool[]> maze)
     {
         _maze = maze;
-        _currentPosition = startPosition;
+        _currentPosition = (1, 1);
+        Console.WriteLine($"Maze created. Start at {_currentPosition}");
     }
 
     public (int x, int y) CurrentPosition => _currentPosition;
 
+    public string GetStatus() => $"Current location (x={_currentPosition.x}, y={_currentPosition.y})";
+
     public void MoveLeft()
     {
-        if (_maze[_currentPosition].Left)
-        {
-            _currentPosition = (_currentPosition.x - 1, _currentPosition.y);
-        }
+        Console.WriteLine($"Attempting MoveLeft from {_currentPosition}. Left allowed: {_maze[_currentPosition][0]}");
+        if (!_maze[_currentPosition][0])
+            throw new InvalidOperationException("Can't go that way!");
+        _currentPosition = (_currentPosition.x - 1, _currentPosition.y);
+        Console.WriteLine($"Moved to {_currentPosition}");
     }
 
     public void MoveRight()
     {
-        if (_maze[_currentPosition].Right)
-        {
-            _currentPosition = (_currentPosition.x + 1, _currentPosition.y);
-        }
+        Console.WriteLine($"Attempting MoveRight from {_currentPosition}. Right allowed: {_maze[_currentPosition][1]}");
+        if (!_maze[_currentPosition][1])
+            throw new InvalidOperationException("Can't go that way!");
+        _currentPosition = (_currentPosition.x + 1, _currentPosition.y);
+        Console.WriteLine($"Moved to {_currentPosition}");
     }
 
     public void MoveUp()
     {
-        if (_maze[_currentPosition].Up)
-        {
-            _currentPosition = (_currentPosition.x, _currentPosition.y + 1);
-        }
+        Console.WriteLine($"Attempting MoveUp from {_currentPosition}. Up allowed: {_maze[_currentPosition][2]}");
+        if (!_maze[_currentPosition][2])
+            throw new InvalidOperationException("Can't go that way!");
+        _currentPosition = (_currentPosition.x, _currentPosition.y + 1);
+        Console.WriteLine($"Moved to {_currentPosition}");
     }
 
     public void MoveDown()
     {
-        if (_maze[_currentPosition].Down)
-        {
-            _currentPosition = (_currentPosition.x, _currentPosition.y - 1);
-        }
+        Console.WriteLine($"Attempting MoveDown from {_currentPosition}. Down allowed: {_maze[_currentPosition][3]}");
+        if (!_maze[_currentPosition][3])
+            throw new InvalidOperationException("Can't go that way!");
+        _currentPosition = (_currentPosition.x, _currentPosition.y - 1);
+        Console.WriteLine($"Moved to {_currentPosition}");
     }
 }
